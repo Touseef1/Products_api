1fe1033a489a29c3750087157eede762e6ac1e40
Touseef <touseef.ahmad@otifcrew.com>
2024-04-12 17:23:50 +0500

    last changing

--- src/main.js
+++ src/main.js
@@ -3,9 +3,7 @@ import store from './store';
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-
 const app = createApp(App)
-
 app.use(router)
 app.use(store)
 
