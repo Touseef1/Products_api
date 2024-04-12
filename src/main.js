7eab4e6a46e4b3a2d639b95e0687e0dcb6139fb1
Touseef <touseef.ahmad@otifcrew.com>
2024-04-12 16:42:23 +0500

    again commit

--- src/main.js
+++ src/main.js
@@ -1,9 +1,9 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.min.js'
-import store from './store'; 
+import store from '@store/store'; 
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import App from '@views/App.vue'
+import router from '@router/router'
 
 const app = createApp(App)
 
